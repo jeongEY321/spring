b8df65aa751552d9a2d18e9a0f04e8394ed8e103
JeongEY321 <kingship89@naver.com>
2023-05-11 18:11:03 +0900

    2023/05/11 redirect까지

---
+++ SpringWebBasic/src/main/java/com/spring/basic/model/UserVO.java
@@ -0,0 +1,20 @@
+package com.spring.basic.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode
+public class UserVO {
+	
+	private String userID;
+	private String suerPw;
+	private String userName;
+	private List<String> hobby;
+	
+}
