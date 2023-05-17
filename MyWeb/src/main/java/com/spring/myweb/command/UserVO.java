85815c039bc817affeb824607f8d40e59b8b5bea
JeongEY321 <kingship89@naver.com>
2023-05-17 19:01:29 +0900

    2023/05/17

---
+++ MyWeb/src/main/java/com/spring/myweb/command/UserVO.java
@@ -0,0 +1,24 @@
+package com.spring.myweb.command;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class UserVO {
+	
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userPhone1;
+	private String userPhone2;
+	private String userEmail1;
+	private String userEmail2;
+	private String addrBasic;
+	private String addrDetail;
+	private String addrZipNum;
+	private LocalDateTime regDate;
+	
+}
