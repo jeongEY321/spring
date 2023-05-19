74f72b3e32672e3dd0fea756b8865ee622a1a301
JeongEY321 <kingship89@naver.com>
2023-05-19 18:10:44 +0900

    2023/05/19

--- MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
+++ MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
@@ -2,13 +2,17 @@ package com.spring.myweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.myweb.command.UserVO;
 import com.spring.myweb.user.service.IUserService;
+import com.spring.myweb.util.MailSenderService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,6 +23,8 @@ public class UserController {
 
 	@Autowired
 	private IUserService service;
+	@Autowired
+	private MailSenderService mailService;
 	
 	//회원가입 페이지로 이동
 	@GetMapping("/userJoin")
@@ -43,10 +49,34 @@ public class UserController {
 	@ResponseBody
 	public String mailCheck(String email) {
 		log.info("이메일 인증 요청 들어옴: " + email);
-			return null;
+		
+		return mailService.joinEmail(email);
 	}
 	
+	//회원가입 처리
+	@PostMapping("/join")
+	public String join(UserVO vo, RedirectAttributes ra) {
+		log.info("param: "+vo);
+		service.join(vo);
+		ra.addFlashAttribute("msg", "joinSuccess");
+		return "redirect:/user/userLogin";
+	}
 	
+	//로그인 페이지로 이동 요청
+	@GetMapping("/userLogin")
+	public void login() {}
 	
+	//로그인 요청
+	@PostMapping("/userLogin")
+	public void login(String userId, String userPw, Model model) {
+		log.info("userController의 login 입니다.");
+		model.addAttribute("user", service.login(userId, userPw));
+	}
+	
+	//마이페이지 이동 요청
+	@GetMapping("/userMypage")
+	public void userMypage() {
+		
+	}
 	
 }
