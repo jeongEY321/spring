23fc15b48be5b5c3e12b7e1f0cf52b6b32b91b76
JeongEY321 <kingship89@naver.com>
2023-05-23 19:18:11 +0900

    223/50/23

--- MyWeb/src/main/java/com/spring/myweb/reply/service/ReplyService.java
+++ MyWeb/src/main/java/com/spring/myweb/reply/service/ReplyService.java
@@ -6,23 +6,24 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.spring.myweb.command.ReplyVO;
 import com.spring.myweb.reply.mapper.IReplyMapper;
 import com.spring.myweb.util.PageVO;
 
+@Service
 public class ReplyService implements IReplyService {
 
 	@Autowired
 	private IReplyMapper mapper;
-	
+	@Autowired
 	private BCryptPasswordEncoder encoder;
 	
 	@Override
 	public void replyRegist(ReplyVO vo) {
-		vo.setReply(encoder.encode(vo.getReplyPw()));
+		vo.setReplyPw(encoder.encode(vo.getReplyPw()));
 		mapper.replyRegist(vo);
-
 	}
 
 	@Override
@@ -30,36 +31,35 @@ public class ReplyService implements IReplyService {
 		
 		PageVO vo = new PageVO();
 		vo.setPageNum(pageNum); //화면에서 전달된 페이지 번호
-		vo.setCpp(5); //댓글은 한 화면에 개씩.
+		vo.setCpp(5); //댓글은 한 화면에 5개씩.
 		
 		Map<String, Object> data = new HashMap<>();
 		data.put("paging", vo); //페이징 쿼리를 위한 pageNum과 cpp
 		data.put("bno", bno); //몇 번 글의 댓글을 가져올지에 대한 정보
-		
 		return mapper.getList(data);
 	}
 
 	@Override
 	public int getTotal(int bno) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mapper.getTotal(bno);
 	}
 
 	@Override
 	public boolean pwCheck(ReplyVO vo) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		String dbPw = mapper.pwCheck(vo.getRno());
+		return encoder.matches(vo.getReplyPw(), dbPw);
 	}
 
 	@Override
 	public void update(ReplyVO vo) {
-		// TODO Auto-generated method stub
+		mapper.update(vo);
 
 	}
 
 	@Override
 	public void delete(int rno) {
-		// TODO Auto-generated method stub
+		mapper.delete(rno);
 
 	}
 
